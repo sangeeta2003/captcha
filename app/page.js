241318c44e48f90419b360244b5631f619c2bfb7
sangeeta2003 <mishrasangeeta944@gmail.com>
2023-11-24 22:27:33 +0530

    export captcha

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 import Image from 'next/image'
-
+import captcha from '@/component/captcha'
 export default function Home() {
   return (
     <main>
       <input type='text' placeholder='Message'/>
-      <div>captcha</div>
+      <div><captcha/></div>
       <button>
         send
       </button>
